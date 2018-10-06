32f2cd970e458bc97c91233cfb523d5d39960cd4
Byronlee <lbyronlee@gmail.com>
2018-10-06 21:22:27 +0800

    奖品介绍

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
   resources :users
+  resources :dashboard do 
+  	get :gifts, on: :collection
+  	get :start, on: :collection  	
+  end
 
-  root 'users#new'
+  root 'dashboard#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
