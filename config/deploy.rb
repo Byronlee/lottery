0b105f18d1d4de9df330ffaa92e4d34b5bd5e868
Byronlee <lbyronlee@gmail.com>
2018-10-07 18:08:08 +0800

    修正部署脚步

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,9 +2,7 @@ require "active_support/all"
 
 #set :scm, :git
 set :application, 'blocktrending'
-set :repo_url, "https://github.com/NoonTechnology/BlockTrending.git"
-
-set :sidekiq_role, [:db, filter: :sidekiq, properties: {}]
+set :repo_url, "https://github.com/Byronlee/lottery.git"
 
 fetch(:linked_files).concat %w[
   config/settings.local.rb
@@ -16,31 +14,6 @@ fetch(:linked_files).concat %w[
 ]
 
 fetch(:linked_dirs).concat %w[
-  public/uploads
   app/assets/bundles
   vendor/node_modules
-  public/downloads
-  public/static/css
-  public/static/js
-  public/static/media
 ]
-
-set :bundle_flags, ""
-
-namespace :deploy do
-  desc "rake db:seed"
-  task :seed do
-    on primary(:db) do
-      within release_path do
-        with rails_env: fetch(:rails_env) do
-          execute :rake, "db:seed"
-        end
-      end
-    end
-  end
-  after "deploy:migrate", "deploy:seed"
-end
-
-load "config/deploy/recipes/webpack.rb"
-load "config/deploy/recipes/notification.rb"
-
