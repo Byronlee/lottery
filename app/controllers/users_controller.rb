a68e7bc4fbd2c0c03ba4bd4db1f08f8687c514b1
Byronlee <lbyronlee@gmail.com>
2018-10-10 12:39:05 +0800

    完成开发

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     if params[:name].blank?
       flash[:not_right_name] = '请输入正确的姓名'
       return redirect_to :back
-    elsif !(/^[1][0-9]{10}$|^([5|6|9])\d{7}$|^[0][9]\d{8}$|^[6]([8|6])\d{5}$/i =~ params[:phone])
+    elsif params[:name].blank?
       flash[:not_right_phone] = '请输入正确的手机号码'
       return redirect_to :back
     end
@@ -23,6 +23,6 @@ class UsersController < ApplicationController
     end
 
   	user = User.create(name: params[:name], phone: params[:phone])
-    redirect_to users_success_path
+    redirect_to success_users_path
   end
 end
